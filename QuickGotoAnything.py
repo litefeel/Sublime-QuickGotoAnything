4cc6df9c96cbc9e0c4179c6b394f103c04f331fa
lite3 <lite3@qq.com>
2014-01-20 16:27:53 +0800

    selected word must has a-zA-Z

--- QuickGotoAnything.py
+++ QuickGotoAnything.py
@@ -1,5 +1,6 @@
 import sublime
 import sublime_plugin
+import re
 
 class QuickGotoCommand(sublime_plugin.TextCommand):
     def doCommand(self, edit, prifix):
@@ -7,6 +8,8 @@ class QuickGotoCommand(sublime_plugin.TextCommand):
             if sel.empty():
                 sel = self.view.word(sel)
             word_sel = self.view.substr(sel)
+            if not re.search('[a-zA-Z]', word_sel):
+                word_sel = ''
             self.view.window().run_command("show_overlay", {"overlay": "goto", "text": prifix+word_sel})
 
 class QuickGotoFunctionCommand(QuickGotoCommand):
