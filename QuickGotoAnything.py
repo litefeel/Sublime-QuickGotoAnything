5e937387a97b0607df52b644ce0665db362deeb1
lite3 <lite3@qq.com>
2014-01-20 15:27:07 +0800

    add quick goto anything

---
+++ QuickGotoAnything.py
@@ -0,0 +1,23 @@
+import sublime
+import sublime_plugin
+
+class QuickGotoCommand(sublime_plugin.TextCommand):
+    def doCommand(self, edit, prifix):
+        for sel in self.view.sel():
+            if sel.empty():
+                sel = self.view.word(sel)
+            word_sel = self.view.substr(sel)
+            self.view.window().run_command("show_overlay", {"overlay": "goto", "text": prifix+word_sel})
+
+class QuickGotoFunctionCommand(QuickGotoCommand):
+    def run(self, edit):
+        self.doCommand(edit, '@')
+        
+class QuickGotoVariableCommand(QuickGotoCommand):
+    def run(self, edit):
+        self.doCommand(edit, '#')
+        
+class QuickGotoFileCommand(QuickGotoCommand):
+    def run(self, edit):
+        self.doCommand(edit, '')
+        
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 Sublime-GoToFunction
 ====================
 
-Sublime Text Plugin to go to the declare of function and the highlighted name
+Sublime Text Plugin to quick scroll to function/variable or quick open file, It use selected word. Works in ST2 and ST3.
+
+### Keybindings
+    goto function:
+    Windows/Linux   ctrl+r
+    OSX             super+r
+    
+    goto variable:
+    Windows/Linux   ctrl+;
+    OSX             super+;
+    
+    goto file:
+    Windows/Linux   ctrl+p
+    OSX             super+p
+
