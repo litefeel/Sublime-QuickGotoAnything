a3a9f64f825a5bfa334fee73c12cc84b5caa7dcc
lite3 <lite3@qq.com>
2014-01-21 22:39:32 +0800

    add regex of every type

--- QuickGotoAnything.py
+++ QuickGotoAnything.py
@@ -3,24 +3,24 @@ import sublime_plugin
 import re
 
 class QuickGotoCommand(sublime_plugin.TextCommand):
-    def doCommand(self, edit, prifix):
+    def doCommand(self, edit, prifix, reg):
         for sel in self.view.sel():
             if sel.empty():
                 sel = self.view.word(sel)
             word_sel = self.view.substr(sel)
-            if not re.search('[a-zA-Z]', word_sel):
+            if not re.search(reg, word_sel):
                 word_sel = ''
             self.view.window().run_command("show_overlay", {"overlay": "goto", "text": prifix+word_sel})
 
 class QuickGotoFunctionCommand(QuickGotoCommand):
     def run(self, edit):
-        self.doCommand(edit, '@')
+        self.doCommand(edit, '@', '^[a-zA-Z_]+[a-zA-Z0-9_]+$')
         
 class QuickGotoVariableCommand(QuickGotoCommand):
     def run(self, edit):
-        self.doCommand(edit, '#')
+        self.doCommand(edit, '#', '^[a-zA-Z_]+[a-zA-Z0-9_]+$')
         
 class QuickGotoFileCommand(QuickGotoCommand):
     def run(self, edit):
-        self.doCommand(edit, '')
+        self.doCommand(edit, '', '^[a-zA-Z0-9_\-\. ]+$')
         
